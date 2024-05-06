520f5e59a74b91740e45b2959865556c4aafb18e
Ethan Rozak <ethanrozak@login3.ls6.tacc.utexas.edu>
2024-05-06 15:29:16 -0500

    moved gold icoFoam case to gold folder

--- tests/writers/test_OpenFoamWriter.py
+++ tests/writers/test_OpenFoamWriter.py
@@ -7,19 +7,18 @@ from flowforge.input.Components import component_factory
 from flowforge.writers.OpenfoamWriter import OpenFoamWriter
 
 def test_single_segment():
-    pass
-#    inputfile = "testOpenFoamWriter/single_segment/system.json"
-#    with open(inputfile, 'r') as rf:
-#        input_dict = json.load(rf)
-#
-#    components = component_factory(input_dict['components'])
-#    system = System(components, input_dict.get('system', {}), input_dict.get('units', {}))
-#
-#    writer = OpenFoamWriter()
-#    writer.write(system, "test")
-#
-#    test_dir = "test"
-#    gold_dir = "gold/test_segment/test_single_segment/openfoam"
-#    assert(outputs_match(test_dir, gold_dir))
-#    
-#    os.remove(test_dir)  
+    inputfile = "testOpenFoamWriter/single_segment/system.json"
+    with open(inputfile, 'r') as rf:
+        input_dict = json.load(rf)
+
+    components = component_factory(input_dict['components'])
+    system = System(components, input_dict.get('system', {}), input_dict.get('units', {}))
+
+    writer = OpenFoamWriter()
+    writer.write(system, "test")
+
+    test_dir = "test"
+    gold_dir = "testOpenFoamWriter/single_segment/gold"
+    assert(outputs_match(test_dir, gold_dir))
+    
+    os.remove(test_dir)  
