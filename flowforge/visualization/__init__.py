9f9ab7d082b206f6ae8d3f907aa892e0acd94582
MicahBest <micahbest21@gmail.com>
2024-08-05 11:27:25 -0500

    Adds visualization features

--- flowforge/visualization/__init__.py
+++ flowforge/visualization/__init__.py
@@ -5,3 +5,24 @@ from .VTKShapes.cube import genUniformCube, genNonuniformCube
 from .VTKShapes.cylinder import genCyl
 from .VTKShapes.nozzle import genNozzle
 from .VTKShapes.trigrid import genTriGrid
+import pyevtk
+
+VtkVertex = pyevtk.vtk.VtkVertex
+VtkPolyVertex = pyevtk.vtk.VtkPolyVertex
+VtkLine = pyevtk.vtk.VtkLine
+VtkPolyLine = pyevtk.vtk.VtkPolyLine
+VtkTriangle = pyevtk.vtk.VtkTriangle
+VtkTriangleStrip = pyevtk.vtk.VtkTriangleStrip
+VtkPolygon = pyevtk.vtk.VtkPolygon
+VtkPixel = pyevtk.vtk.VtkPixel
+VtkQuad = pyevtk.vtk.VtkQuad
+VtkTetra = pyevtk.vtk.VtkTetra
+VtkVoxel = pyevtk.vtk.VtkVoxel
+VtkHexahedron = pyevtk.vtk.VtkHexahedron
+VtkWedge = pyevtk.vtk.VtkWedge
+VtkPyramid = pyevtk.vtk.VtkPyramid
+VtkQuadraticEdge = pyevtk.vtk.VtkQuadraticEdge
+VtkQuadraticTriangle = pyevtk.vtk.VtkQuadraticTriangle
+VtkQuadraticQuad = pyevtk.vtk.VtkQuadraticQuad
+VtkQuadraticTetra = pyevtk.vtk.VtkQuadraticTetra
+VtkQuadraticHexahedron = pyevtk.vtk.VtkQuadraticHexahedron
