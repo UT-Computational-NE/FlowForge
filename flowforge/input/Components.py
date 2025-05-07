c9170f69435c8bd95d6908296421004fabc4c0cc
npl436 <nicholas.luciano@austin.utexas.edu>
2025-05-07 19:00:11 -0400

    Addressed pylint issue

--- flowforge/input/Components.py
+++ flowforge/input/Components.py
@@ -1818,9 +1818,10 @@ class HexCore(Core):
             expected = ascending + descending
         else:
             # For x-oriented hexagon (pointy-top)
-            assert (
-                num_rows - 1
-            ) % 4 == 0, f"for y orientation, channel_map must have 4n + 1 number of rows (i.e., 1, 5, 9, 13, 17, ...), not {num_rows} rows"
+            assert (num_rows - 1) % 4 == 0, (
+                "for y orientation, channel_map must have 4n + 1 number of rows "
+                f"(i.e., 1, 5, 9, 13, 17, ...), not {num_rows} rows"
+            )
             n = (num_rows - 1) // 4
             # Ascending
             ascending = list(range(1, n + 2))
