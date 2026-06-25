88bb304f7879afe64a32e9259ebb0a5ba3575026
Charlie <charles.owen@austin.utexas.edu>
2026-06-25 13:17:20 -0500

    Refactored system to match new input format

--- tests/input/test_BodyForces.py
+++ tests/input/test_BodyForces.py
@@ -1,41 +1,75 @@
+import numpy as np
 from flowforge.input.BodyForces import *
-
+from flowforge.input.System import SimulationType
+from flowforge.input.Components import Pipe
 
 def test_GeneralBF():
 
-    bf_solid = GeneralBF("solid_temperature", EquationParser("700"))
+    bf = GeneralBodyForce(EquationParser("700"))
 
-    assert bf_solid.body_force_type is None
-    assert bf_solid.body_force_value.evaluate() == 700
-    assert bf_solid.variable_name == "solid_temperature"
-
+    assert bf.body_force_type is None
+    assert bf.body_force_value.evaluate() == 700
+    assert bf.variable_name == None
+    assert bf.component == None
 
 def test_InternalHeatGenerationBF():
 
-    bf_solid = HeatGenerationBF("solid_temperature", EquationParser("700"))
+    bf = InternalHeatGenerationBF(EquationParser("1e6"))
 
-    assert bf_solid.body_force_type == "heat_generation"
+    assert bf.body_force_type == "InternalHeatGeneration"
+    assert bf.variable_name == "power_density"
 
+def test_DifferentialPressureBF():
+
+    bf = DifferentialPressureBF(EquationParser("1e5"))
+
+    assert bf.body_force_type == "DifferentialPressure"
+    assert bf.variable_name == "pressure"
+
+def test_attachComponent():
+
+    bf = GeneralBodyForce(EquationParser("700"))
+    comp = Pipe(L=1.0, cross_section_name="circular", R=1.0, n=10)
+    bf.attach_component(comp)
+    assert bf.component.length == 1.0
+
+def test_convertUnits():
+
+    bf = InternalHeatGenerationBF(EquationParser("1.0"))
+    units = units = {"length": "cm", "power": "w"}
+    uc = UnitConverter(units)
+    bf.convertUnits(uc)
+    assert np.isclose(bf.body_force_value.evaluate(), 1e6), bf.body_force_value.evaluate()
 
 def test_BodyForces():
 
     body_forces = {
-        "temperature_bf" : {"type": "heat_generation", "variable": "temperature", "value": 700},
-        "enthalpy_bf" : {"type": "heat_generation", "variable": "enthalpy", "value": 1e6}
+        "power_bf" : {"type": "InternalHeatGeneration", "value": 1e6},
+        "pressure_bf" : {"type": "DifferentialPressure", "value": 1e5}
     }
+    comp = Pipe(L=1.0, cross_section_name="circular", R=1.0, n=10)
 
-    BF = BodyForces(**body_forces)
+    BF = BodyForces(SimulationType.FLUID)
+    for name, bf_def in body_forces.items():
+        BF.addBodyForce(name, bf_def, comp)
 
-    for bf_name, bf in BF.body_forces.items():
-        assert bf.body_force_type == body_forces[bf_name]["type"]
-        assert bf.variable_name == body_forces[bf_name]["variable"]
-        assert bf.body_force_value.evaluate() == body_forces[bf_name]["value"]
+    assert BF.body_forces["power_bf_0"].body_force_type == "InternalHeatGeneration"
+    assert BF.body_forces["power_bf_0"].variable_name == "power_density"
+    assert BF.body_forces["power_bf_0"].body_force_value.evaluate() == 1e6
+    assert BF.body_forces["power_bf_0"].component == comp
 
-    return
+    assert BF.body_forces["pressure_bf_0"].body_force_type == "DifferentialPressure"
+    assert BF.body_forces["pressure_bf_0"].variable_name == "pressure"
+    assert BF.body_forces["pressure_bf_0"].body_force_value.evaluate() == 1e5
+    assert BF.body_forces["pressure_bf_0"].component == comp
 
 
 if __name__ == "__main__":
     test_GeneralBF()
     test_InternalHeatGenerationBF()
+    test_DifferentialPressureBF()
+
+    test_attachComponent()
+    test_convertUnits()
 
     test_BodyForces()
